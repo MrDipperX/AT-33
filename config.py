778a2dc9a0ff582e65fac8c59c5ca0c4f48a3c51
MrDipper <Naruhina5>
2022-03-01 23:45:24 +0500

    configure db credentials

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ TOKEN = "5163452568:AAHV1YwHWJTlsk0s_v7qsv0pIGE2LE2FTg8"
 # port = 5432
 # password = "Naruhina"
 
-host = "ec2-100-24-139-146.compute-1.amazonaws.com"
-dbname = "d352kabd9ideph"
-user = "avntzjmffgqakb"
+host = "ec2-3-228-222-169.compute-1.amazonaws.com"
+dbname = "d9ocvq1c3g3kb5"
+user = "zqvalpezikoltp"
 port = 5432
-password = "18f4bce32d0c15809f9111104eb63299516b9d372c0fc41e64338631fa8b9c89"
+password = "5aee283d1dc2e0550deb362e61d8c5fbb00e1ef15d50637482692f8521869b07"
 
 
 city_id = 1512569
